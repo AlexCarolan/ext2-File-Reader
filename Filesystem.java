667837f7744d4ed4b0b6dcbaa6eef09419dc73bc
Carolan <carolana@lancs.local>
2017-11-29 13:56:21 +0000

    Added functionalty to the Helper class - now prints byte array

--- Filesystem.java
+++ Filesystem.java
@@ -4,16 +4,14 @@ public class Filesystem
 	{
 		//Test.txt
 		//ext2fs
+		Helper helper = new Helper();
 		
 		Volume vol = new Volume("H:\\Computer Science\\Second Year\\SCC.211 Operating Systems\\Week 5 - 10\\files\\ext2fs");
 		Ext2File file = new  Ext2File(vol);
 		
-		byte buf[ ] = file.read(0L, file.size());
+		byte buf[ ] = file.read(0L, 997);
 		
-		for(int i = 0; i < file.size(); i++)
-		{
-			System.out.print(buf[i] + " ");
-		}
+		helper.dumpHexBytes(buf);
 		
 	}
 }
--- Helper.java
+++ Helper.java
@@ -1,15 +1,70 @@
 public class Helper
-{
-	private Ext2File file;
-	
-	public Helper(Ext2File f)
-	{
-		file = f;
-	}
-	
+{	
+	//Prints out the contents as a byte array in the format of hex and chars
 	public void dumpHexBytes(byte[] bytes)
 	{
-		
+		int offset = 0;
+		while(offset <= bytes.length)
+		{
+			for(int i = offset; i < offset+8; i++)
+			{	
+				if(i < bytes.length)
+				{
+					System.out.printf("%02X ", bytes[i]);
+				}else{
+					System.out.print("XX ");
+				}
+			}
+			
+			System.out.print("| ");
+			
+			for(int i = offset+8; i < offset+16; i++)
+			{
+				if(i < bytes.length)
+				{
+					System.out.printf("%02X ", bytes[i]);
+				}else{
+					System.out.print("XX ");
+				}
+			}
+			
+			System.out.printf("| ");
+			
+			for(int i = offset; i < offset+8; i++)
+			{
+				if(bytes.length > i)
+				{
+					if (bytes[i] > 31 & bytes[i] < 123)
+					{
+						System.out.print((char)bytes[i]);
+					}else{
+						System.out.print("-");
+					}
+				}else{
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.printf(" | ");
+			
+			for(int i = offset+8; i < offset+16; i++)
+			{
+				if(bytes.length > i)
+				{
+					if (bytes[i] > 31 & bytes[i] < 123)
+					{
+						System.out.print((char)bytes[i]);
+					}else{
+						System.out.print("-");
+					}
+				}else{
+					System.out.print(" ");
+				}
+			}
+			
+			System.out.printf("\n");
+			offset = offset + 16;
+		}
 	}
 	
 	
