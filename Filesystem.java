d1116e62e7ea63ac3234f30c0eda5abf2c78063b
Carolan <carolana@egb076000116.lancs.local>
2017-11-30 13:50:04 +0000

    Added support for both unix and windows filepaths

--- Filesystem.java
+++ Filesystem.java
@@ -2,16 +2,27 @@ public class Filesystem
 {
 	public static void main(String[] args)
 	{
+		String winStr = "H:\\Computer Science\\Second Year\\SCC.211 Operating Systems\\Week 5 - 10\\files\\ext2fs";
+		String unixStr = "/home/lancs/carolana/hdrive/Computer Science/Second Year/SCC.211 Operating Systems/Week 5 - 10/repo/ext2-File-Reader/files/ext2fs";
+
+		
 		//Test.txt
 		//ext2fs
 		Helper helper = new Helper();
+		Volume vol;
 		
-		Volume vol = new Volume("H:\\Computer Science\\Second Year\\SCC.211 Operating Systems\\Week 5 - 10\\files\\ext2fs");
+		if (System.getProperty("os.name").startsWith("Windows")) {
+			vol = new Volume(winStr);
+		} else {
+			vol = new Volume(unixStr);
+		}
+		
+		
+		
 		Ext2File file = new  Ext2File(vol);
 		
-		byte buf[] = file.read(1024,500);	
 		
-		
+		byte buf[] = file.read(2048, 1000);
 		
 		helper.dumpHexBytes(buf);
 		
