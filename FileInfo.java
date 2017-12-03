7f463b69f78b7d0a05328772a416a1cdd122625a
Alex Carolan <AlexCarolan0@gmail.com>
2017-12-03 14:29:31 +0000

    Added ability to read data from the file in the FileInfo class, also now able to output the files name

--- FileInfo.java
+++ FileInfo.java
@@ -1,27 +1,63 @@
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Date;
-import java.util.HashMap;
 public class FileInfo
 {
 	private Ext2File file;
+	private ByteBuffer byteBuff;
+	private int offset;
 	private int inodeNumber;
 	private int length;
-	private int nameLength
-	private int fileType
+	private int nameLength;
+	private int fileType;
 	private String fileName;
-	
 	
 	
 	public FileInfo (Ext2File f, int o)
 	{
 		file = f;
+		offset = o;
+		
+		byte buffer[] = file.read(offset, 4);
+		byteBuff = ByteBuffer.wrap(buffer);
+		byteBuff.order(ByteOrder.LITTLE_ENDIAN);
+		inodeNumber = byteBuff.getInt();
+		
+		buffer = file.read(offset+4, 2);
+		byte paddedArray[] = {0,0,0,0};
+		System.arraycopy(buffer, 0, paddedArray, 0, 2);
+		byteBuff = ByteBuffer.wrap(paddedArray);
+		byteBuff.order(ByteOrder.LITTLE_ENDIAN);
+		length = byteBuff.getInt();
+		
+		buffer = file.read(offset+6, 1);
+		System.arraycopy(buffer, 0, paddedArray, 0, 1);
+		byteBuff = ByteBuffer.wrap(paddedArray);
+		byteBuff.order(ByteOrder.LITTLE_ENDIAN);
+		nameLength = byteBuff.getInt();
+		
+		buffer = file.read(offset+7, 1);
+		System.arraycopy(buffer, 0, paddedArray, 0, 1);
+		byteBuff = ByteBuffer.wrap(paddedArray);
+		byteBuff.order(ByteOrder.LITTLE_ENDIAN);
+		fileType = byteBuff.getInt();
+		
+		//Calculate the number of 4 byte blocks that hold the file name
+		int nameBytes = nameLength/4;
+		if((nameLength%4) != 0)
+		{
+			nameBytes++;
+		}
+		
+		buffer = file.read(offset+8, (nameBytes*4));
+		byteBuff = ByteBuffer.wrap(buffer);
+		byteBuff.order(ByteOrder.LITTLE_ENDIAN);
+		fileName = new String(byteBuff.array());
+		
 	}
 
-	public void printFileInfo()
+	public void printFileName()
 	{
-		
-		
+		System.out.print(fileName);
 	}
 
 }
